66a51a272d9291c08dde66a6f970816902f4aced
Manuel John <manuelmathew39@gmail.com>
2024-07-06 13:15:48 +0530

    Added Setup.py

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Here, we will be walk through end to end implementation of a Machine Learning Pi
 
 Initial setup to do:
 1. Set up Github repo
-
    
 	a. New Environment —> in VS CODE itself (inside the project folder)
 
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+
+PRJT_NAME = 'sample-ml-pipeline'
+VERSION = '0.0.1'
+AUTHOR = 'Manuel John'
+EMAIL = 'manuelmathew39@gmail.com'
+
+
+def get_requirements(file_path: str) -> list[str]:
+    """
+    This Function gets all the required libraries
+    required in the project
+    """
+
+    HYPEN_E_DOT = "-e ."
+
+    requirements = []
+
+    with open(file=file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", ' ') 
+                        for req in requirements
+                        ]                       #replace \n from file
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    print("REQ")
+
+    return requirements
+        
+setup(name=PRJT_NAME, 
+      version=VERSION, 
+      author=AUTHOR, 
+      author_email=EMAIL, 
+      packages=find_packages(), 
+      install_requires=get_requirements(file_path='requirements.txt')
+      )
