bb85b414c5c1f4c4c037d45cbe499c9185cbfbe3
manuelmj39 <manuelmathew39@gmail.com>
2024-08-22 19:49:44 +0530

    Adding Docstring

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,48 +1,90 @@
+"""This Module consists of Data Ingestion Pipeline"""
 import os
 import sys
+
 sys.path.append(r"D:\Training\ml-pipeline")
 
+import pandas as pd
 import yaml
 
-import pandas as pd
-
-from src.logger import logging
 from src.exception import CustomException
+from src.logger import logging
 
 # folder to load config file
 CONFIG_PATH = "config/"
 
 # Function to load yaml configuration file
 def load_config(config_name):
-    with open(os.path.join(CONFIG_PATH, config_name)) as file:
-        config = yaml.safe_load(file)
+    """
+    Load configuration file
+
+    This function load the configuration file which
+    consists of all the paths, and other miscellaneous
+    contents.
+
+    Parameters
+    ----------
+        config_path: str
+            Path to the configuration file
+
+    Returns
+    -------
+        dict
+            Dictionary of configurations
+    """
+    with open(os.path.join(CONFIG_PATH, config_name)) as config_file:
+        config = yaml.safe_load(config_file)
 
     return config
 
-config = load_config("main_conf.yaml")
+
+config = load_config(config_name="main_conf.yaml")
+
 
 class DataIngestion:
-    def __init__(self):
-        self.raw_bronze_data_path = config['data']['raw_bronze']
-        self.raw_silver_data_path = config['data']['raw_silver']
+    """
+    Class for Data Ingestion Process
+    """
 
-    def initiate_data_ingestion(self):
+    def __init__(self):
+        self.raw_bronze_data_path = config["data"]["raw_bronze"]
+        self.raw_silver_data_path = config["data"]["raw_silver"]
+
+    def initiate_data_ingestion(self) -> str:
+        """ "
+        Inititate data Ingestion
+
+        This function initiates the data Ingestion
+        process and stores the data in the desired path
+        and finally return path location where the data is
+        stored
+
+        Raises
+        ------
+            CustomException: Exception
+                Some Custom Exception
+
+        Returns
+        -------
+            str
+                Path where the file got stored
+        """
         logging.info("Starting ingestion of data")
 
         try:
             df = pd.read_csv(self.raw_bronze_data_path)
             logging.info("Loaded the CSV File")
 
-            df.to_csv(self.raw_silver_data_path, 
-                      index=False, header=True)
-            
+            df.to_csv(self.raw_silver_data_path, index=False, header=True)
+
             logging.info("Completed Ingestion Process")
 
-            return (self.raw_silver_data_path)
+            return self.raw_silver_data_path
 
         except Exception as e:
             logging.info(e)
             raise CustomException(e, sys)
+
 
 data_ingest_obj = DataIngestion()
 data_ingest_obj.initiate_data_ingestion()
